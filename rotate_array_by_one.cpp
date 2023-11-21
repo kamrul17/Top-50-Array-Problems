6fd292f67c539d92e9d9044c41a5c0e5cd84f3ad
kamrul17 <72572933+kamrul17@users.noreply.github.com>
2023-11-21 19:39:49 +0530

    solve 1st problem

---
+++ rotate_array_by_one.cpp
@@ -0,0 +1,25 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+void solve(int arr[], int n)
+{
+    int temp = arr[n - 1];
+    for (int i = n - 1; i > 0; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = temp;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int n = 5;
+
+    int arr[] = {1, 2, 3, 4, 5};
+    solve(arr, n);
+}
