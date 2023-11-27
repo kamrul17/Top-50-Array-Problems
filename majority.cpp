51911caeffb5b1e28113376926f4cb68b8d76b1a
kamrul17 <72572933+kamrul17@users.noreply.github.com>
2023-11-27 19:59:33 +0530

    upd

--- majority.cpp
+++ majority.cpp
@@ -1,3 +1,6 @@
+/*
+Given an array A of N elements. Find the majority element in the array. A majority element in an array A of size N is an element that appears strictly more than N/2 times in the array.
+ */
 #include <bits/stdc++.h>
 using namespace std;
 // brute force
