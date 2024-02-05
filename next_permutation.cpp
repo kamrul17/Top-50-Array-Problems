3ac26cf41a480ac825bfbf87be698c0e5d44c97d
kamrul17 <72572933+kamrul17@users.noreply.github.com>
2024-02-05 21:52:56 +0530

    solved

---
+++ next_permutation.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 3, 2};
+
+    next_permutation(arr, arr + 3);
+
+    cout << arr[0] << " " << arr[1] << " " << arr[2];
+
+    return 0;
+}
